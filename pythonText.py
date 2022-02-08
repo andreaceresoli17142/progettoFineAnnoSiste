33af773a3fde60f4f3cea673d8f7f613da37e84d
andreaceresoli17142 <andrea.ceresoli03@gmail.com>
2022-02-08 09:22:38 +0100

    automated commit 08/02/2022 09:22

--- pythonText.py
+++
@@ -1,9 +0,0 @@
-from mariadbInterface import mariaDbInterface
-
-dbi = mariaDbInterface("instanTex_db")
-
-def main():
-    print(dbi.query("SELECT * FROM Users;"))
-
-if __name__ == "__main__":
-    main()
